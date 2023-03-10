31e4233318d32a5438191d5af5c3b03cc0e7e66d
giuseppe romano <giuseppenadir.romano@gmail.com>
2023-03-10 18:08:58 +0100

    add push detection strategy on product list

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,59 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 import { MatButtonToggleChange } from '@angular/material/button-toggle';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { State } from '../state/product.reducer';
 import { Product } from '../product.model';
-import * as ProductActions from '../state/product.actions';
-import {
-  getAreProductLoading,
-  getCurrentProduct,
-  getIsProductFormVisible,
-  getProducts,
-  getShowProductCard,
-} from '../state/product.reducer';
+import { State } from '../state/product.reducer';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductListComponent implements OnInit {
-  products$!: Observable<Product[]>;
-  areProductLoading$!: Observable<boolean>;
-  showProductCard$!: Observable<boolean>;
-  currentProduct$!: Observable<Product | null | undefined>;
-  isProductFormVisible$!: Observable<boolean>;
-
+export class ProductListComponent {
+  @Input() products!: Product[] | null;
+  @Input() areProductLoading!: boolean | null;
+  @Output() showProductCardChange: EventEmitter<MatButtonToggleChange> =
+    new EventEmitter();
+  @Output() currentProductSelectChange: EventEmitter<Product> =
+    new EventEmitter();
   constructor(private store: Store<State>) {}
-
-  ngOnInit(): void {
-    this.products$ = this.store.select(getProducts);
-    this.store.dispatch(ProductActions.loadProducts());
-
-    this.areProductLoading$ = this.store.select(getAreProductLoading);
-    this.showProductCard$ = this.store.select(getShowProductCard);
-    this.currentProduct$ = this.store.select(getCurrentProduct);
-    this.isProductFormVisible$ = this.store.select(getIsProductFormVisible);
-  }
+  @Output() onDeleteProduct: EventEmitter<string> = new EventEmitter();
 
   onShowProductCardChange(event: MatButtonToggleChange) {
-    this.store.dispatch(
-      ProductActions.toggleProductCard({ showProductCard: event.value })
-    );
+    this.showProductCardChange.emit(event);
   }
 
   onCurrentProductSelected(product: Product) {
-    this.store.dispatch(
-      ProductActions.setCurrentProduct({ currentProductId: product.id })
-    );
-  }
-
-  onNewProduct() {
-    this.store.dispatch(ProductActions.createNewProduct());
+    this.currentProductSelectChange.emit(product);
   }
 
   deleteProduct(productId: string) {
-    this.store.dispatch(ProductActions.deleteProduct({ productId }));
+    this.onDeleteProduct.emit(productId);
   }
 }
--- src/app/product/product-shell/product-shell.component.ts
+++ src/app/product/product-shell/product-shell.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Product } from '../product.model';
+import {
+  getAreProductLoading,
+  getCurrentProduct,
+  getIsProductFormVisible,
+  getProducts,
+  getShowProductCard,
+  State,
+} from '../state/product.reducer';
+import * as ProductActions from '../state/product.actions';
+import { MatButtonToggleChange } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-product-shell',
   templateUrl: './product-shell.component.html',
-  styleUrls: ['./product-shell.component.scss']
+  styleUrls: ['./product-shell.component.scss'],
 })
 export class ProductShellComponent implements OnInit {
+  products$!: Observable<Product[]>;
+  areProductLoading$!: Observable<boolean>;
+  showProductCard$!: Observable<boolean>;
+  currentProduct$!: Observable<Product | null | undefined>;
+  isProductFormVisible$!: Observable<boolean>;
 
-  constructor() { }
+  constructor(private store: Store<State>) {}
 
   ngOnInit(): void {
+    this.products$ = this.store.select(getProducts);
+    this.store.dispatch(ProductActions.loadProducts());
+
+    this.areProductLoading$ = this.store.select(getAreProductLoading);
+    this.showProductCard$ = this.store.select(getShowProductCard);
+    this.currentProduct$ = this.store.select(getCurrentProduct);
+    this.isProductFormVisible$ = this.store.select(getIsProductFormVisible);
   }
 
+  onShowProductCardChange(event: MatButtonToggleChange) {
+    this.store.dispatch(
+      ProductActions.toggleProductCard({ showProductCard: event.value })
+    );
+  }
+
+  onCurrentProductSelected(product: Product) {
+    this.store.dispatch(
+      ProductActions.setCurrentProduct({ currentProductId: product.id })
+    );
+  }
+
+  onNewProduct() {
+    this.store.dispatch(ProductActions.createNewProduct());
+  }
+
+  deleteProduct(productId: string) {
+    this.store.dispatch(ProductActions.deleteProduct({ productId }));
+  }
 }
